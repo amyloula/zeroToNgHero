345756746f70207cf0162a634d97bd1da3336bb6
amyloula <mccabeamylouise@gmail.com>
2019-11-16 08:09:22 +0000

    feature(app) add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./todos/todos.module').then(m => m.TodosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
